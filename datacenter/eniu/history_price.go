c649972f335d393f3ab338fa7838592899fd6b5c
axiaoxin <254606826@qq.com>
2021-04-24 00:00:08 +0800

    历史波动率

--- datacenter/eniu/history_price.go
+++ datacenter/eniu/history_price.go
@@ -5,6 +5,7 @@ package eniu
 import (
 	"context"
 	"fmt"
+	"math"
 	"strings"
 	"time"
 
@@ -17,6 +18,37 @@ import (
 type RespHistoryStockPrice struct {
 	Date  []string  `json:"date"`
 	Price []float64 `json:"price"`
+}
+
+// HistoryVolatility 按月计算历史波动率
+// 历史波动率计算方法：
+// 1、从市场上获得标的股票在固定时间间隔(如每天、每周或每月等)上的价格。
+// 2、对于每个时间段，求出该时间段末的股价与该时段初的股价之比的自然对数。
+// 3、求出这些对数值的标准差，再乘以一年中包含的时段数量的平方根(如，选取时间间隔为每天，则若扣除闭市，每年中有250个交易日，应乘以根号250)，得到的即为历史波动率。
+func (p RespHistoryStockPrice) HistoryVolatility(ctx context.Context) (float64, error) {
+	// 股价按月分组
+	monthPrices := map[string][]float64{}
+	for i, date := range p.Date {
+		dayIdx := strings.LastIndex(date, "-")
+		key := date[:dayIdx]
+		monthPrices[key] = append(monthPrices[key], p.Price[i])
+	}
+	// 求末初股价比自然对数
+	logs := []float64{}
+	for _, prices := range monthPrices {
+		startPrice := prices[0]
+		endPrice := prices[len(prices)-1]
+		log := math.Log(endPrice / startPrice)
+		logs = append(logs, log)
+	}
+	// 标准差
+	stddev, err := goutils.StdDeviationFloat64(logs)
+	if err != nil {
+		return 0, err
+	}
+	logging.Debugs(ctx, "stddev:", stddev)
+	volatility := stddev * math.Sqrt(12)
+	return volatility, nil
 }
 
 // QueryHistoryStockPrice 获取历史股价
