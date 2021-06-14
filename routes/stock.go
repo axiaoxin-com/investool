d18b7de6a768577c0f25855eacc07d414f51f648
axiaoxin <254606826@qq.com>
2021-06-14 16:34:53 +0800

    checker html

--- routes/stock.go
+++ routes/stock.go
@@ -3,7 +3,9 @@
 package routes
 
 import (
+	"fmt"
 	"net/http"
+	"strings"
 
 	"github.com/axiaoxin-com/x-stock/core"
 	"github.com/axiaoxin-com/x-stock/datacenter/eastmoney"
@@ -41,7 +43,7 @@ func StockSelector(c *gin.Context) {
 	param := ParamStockSelector{}
 	if err := c.ShouldBind(&param); err != nil {
 		data["Error"] = err.Error()
-		c.HTML(http.StatusOK, "stock_index.html", data)
+		c.JSON(http.StatusOK, data)
 		return
 	}
 	var checker core.Checker
@@ -53,7 +55,7 @@ func StockSelector(c *gin.Context) {
 	stocks, err := selector.AutoFilterStocks(c)
 	if err != nil {
 		data["Error"] = err.Error()
-		c.HTML(http.StatusOK, "stock_index.html", data)
+		c.JSON(http.StatusOK, data)
 		return
 	}
 	dlist := models.ExportorDataList{}
@@ -65,12 +67,48 @@ func StockSelector(c *gin.Context) {
 	return
 }
 
+// ParamStockChecker StockChecker 请求参数
+type ParamStockChecker struct {
+	Keyword        string `form:"checker_keyword"`
+	CheckerOptions core.CheckerOptions
+}
+
 // StockChecker 处理个股检测
 func StockChecker(c *gin.Context) {
 	data := gin.H{
 		"PageTitle": "X-STOCK | 股票 | 个股检测",
 		"Error":     "",
 	}
-	c.HTML(http.StatusOK, "stock_index.html", data)
+	param := ParamStockChecker{}
+	if err := c.ShouldBind(&param); err != nil {
+		data["Error"] = err.Error()
+		c.JSON(http.StatusOK, data)
+		return
+	}
+	if param.Keyword == "" {
+		data["Error"] = "请填写股票代码或简称"
+		c.JSON(http.StatusOK, data)
+		return
+	}
+	searcher := core.NewSearcher(c)
+	keywords := strings.Split(param.Keyword, "/")
+	stocks, err := searcher.Search(c, keywords)
+	if err != nil {
+		data["Error"] = err.Error()
+		c.JSON(http.StatusOK, data)
+		return
+	}
+	checker := core.NewChecker(c, param.CheckerOptions)
+	results := []core.CheckResult{}
+	names := []string{}
+	for _, stock := range stocks {
+		result, _ := checker.CheckFundamentals(c, stock)
+		results = append(results, result)
+		name := fmt.Sprintf("%s-%s", stock.BaseInfo.SecurityNameAbbr, stock.BaseInfo.Secucode)
+		names = append(names, name)
+	}
+	data["Results"] = results
+	data["Names"] = names
+	c.JSON(http.StatusOK, data)
 	return
 }
