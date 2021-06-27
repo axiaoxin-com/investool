357be945536eb975bb58de89481cd3a42bbcfb19
axiaoxin <254606826@qq.com>
2021-06-27 11:48:01 +0800

    release script fix

--- release.sh
+++ release.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-echo "Realeasing ..."
-goreleaser release --rm-dist  && qshell batchdelete blogpostpics -y -i /tmp/qiniu-prefetch-del-list.txt
+go mod tidy && \
+go test -race ./... && \
+bash ./misc/scripts/app.min.js.sh && \
+bash ./misc/scripts/bumpversion.sh && \
+goreleaser release --rm-dist  && \
+qshell batchdelete blogpostpics -y -i /tmp/qiniu-del-list.txt
