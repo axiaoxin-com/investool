496d17322c0267b79947f65e28f2cdaf6ade81e3
axiaoxin <254606826@qq.com>
2021-06-27 11:23:06 +0800

    release update

--- release.sh
+++ release.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 echo "Realeasing ..."
-goreleaser release --rm-dist
-qshell batchdelete blogpostpics -y -i /tmp/qiniu-prefetch-del-list.txt
+goreleaser release --rm-dist  && qshell batchdelete blogpostpics -y -i /tmp/qiniu-prefetch-del-list.txt
