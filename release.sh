a0a2e9cbbf224f961c3300dfe7f3063f686d362a
axiaoxin <254606826@qq.com>
2021-06-27 12:02:38 +0800

    push tags

--- release.sh
+++ release.sh
@@ -4,4 +4,5 @@ go test -race ./... && \
 bash ./misc/scripts/app.min.js.sh && \
 bash ./misc/scripts/bumpversion.sh && \
 goreleaser release --rm-dist  && \
-qshell batchdelete blogpostpics -y -i /tmp/qiniu-del-list.txt
+git push --tags && \
+qshell batchdelete blogpostpics -y -i /tmp/qiniu-prefetch-del-list.txt
