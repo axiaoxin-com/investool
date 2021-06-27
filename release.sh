426993bcbebfaa064828aaa2b784705b84c3fd26
axiaoxin <254606826@qq.com>
2021-06-27 12:05:25 +0800

    update script

--- release.sh
+++ release.sh
@@ -4,5 +4,7 @@ go test -race ./... && \
 bash ./misc/scripts/app.min.js.sh && \
 bash ./misc/scripts/bumpversion.sh && \
 goreleaser release --rm-dist  && \
-git push --tags && \
-qshell batchdelete blogpostpics -y -i /tmp/qiniu-prefetch-del-list.txt
+git push --tags -f && \
+qshell cdnprefetch -i /tmp/qiniu-prefetch-refresh-list.txt && \
+qshell cdnrefresh -i /tmp/qiniu-prefetch-refresh-list.txt && \
+qshell batchdelete blogpostpics -y -i /tmp/qiniu-del-list.txt
