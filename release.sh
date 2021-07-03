6a0ccc86cc99ea143eed57c194b927ec9ce6492e
axiaoxin <254606826@qq.com>
2021-07-04 03:27:37 +0800

    echo info

--- release.sh
+++ release.sh
@@ -4,8 +4,13 @@ go test -race ./... && \
 rm -rf ./statics/js/app.min.*.js && \
 bash ./misc/scripts/app.min.js.sh && \
 bash ./misc/scripts/bumpversion.sh && \
+echo "cdn预取..."
 qshell cdnprefetch -i /tmp/qiniu-prefetch-refresh-list.txt && \
+echo "cdn刷新..."
 qshell cdnrefresh -i /tmp/qiniu-prefetch-refresh-list.txt && \
+echo "cdn清理..."
 qshell batchdelete blogpostpics -y -i /tmp/qiniu-del-list.txt && \
+echo "release..."
 goreleaser release --rm-dist  && \
+echo "push..."
 git push && git push --tags -f
