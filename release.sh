cbdc03bf7008835b46479ae6c1943a91c2175b4d
axiaoxin <254606826@qq.com>
2021-07-04 12:42:51 +0800

    release 修复

--- release.sh
+++ release.sh
@@ -4,7 +4,7 @@ go test -race ./... && \
 rm -rf ./statics/js/app.min.*.js && \
 bash ./misc/scripts/app.min.js.sh && \
 bash ./misc/scripts/bumpversion.sh && \
-echo "release..."
-goreleaser release --rm-dist  && \
 echo "push..."
-git push && git push --tags -f
+git push && git push --tags -f && \
+echo "release..."
+goreleaser release --rm-dist
