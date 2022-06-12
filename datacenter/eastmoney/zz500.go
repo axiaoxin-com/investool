e4e37b24fa36cd7263b5d528b15d44ce66e033b1
axiaoxin <254606826@qq.com>
2022-06-12 13:24:41 +0800

    add zz500

--- datacenter/eastmoney/zz500.go
+++ datacenter/eastmoney/zz500.go
@@ -1,4 +1,4 @@
-// 沪深300成分股
+// 中证500成分股
 
 package eastmoney
 
@@ -10,8 +10,8 @@ import (
 	"github.com/corpix/uarand"
 )
 
-// HS300Item 沪深300成分股信息
-type HS300Item struct {
+// ZZ500Item 中证500成分股信息
+type ZZ500Item struct {
 	Secucode         string      `json:"SECUCODE"`           // 股票代码.XX
 	SecurityCode     string      `json:"SECURITY_CODE"`      // 股票代码
 	SecurityNameAbbr string      `json:"SECURITY_NAME_ABBR"` // 股票简称
@@ -30,12 +30,12 @@ type HS300Item struct {
 	F3               interface{} `json:"f3"`
 }
 
-// RspHS300 HS300接口返回结构
-type RspHS300 struct {
+// RspZZ500 ZZ500接口返回结构
+type RspZZ500 struct {
 	Version string `json:"version"`
 	Result  struct {
 		Pages int         `json:"pages"`
-		Data  []HS300Item `json:"data"`
+		Data  []ZZ500Item `json:"data"`
 		Count int         `json:"count"`
 	} `json:"result"`
 	Success bool   `json:"success"`
@@ -43,21 +43,21 @@ type RspHS300 struct {
 	Code    int    `json:"code"`
 }
 
-// HS300 返回沪深300成分股列表
-func (e EastMoney) HS300(ctx context.Context) (results []HS300Item, err error) {
-	apiurl := "https://datacenter-web.eastmoney.com/api/data/v1/get?sortColumns=ROE&sortTypes=-1&pageSize=300&pageNumber=1&reportName=RPT_INDEX_TS_COMPONENT&columns=SECUCODE%2CSECURITY_CODE%2CTYPE%2CSECURITY_NAME_ABBR%2CCLOSE_PRICE%2CINDUSTRY%2CREGION%2CWEIGHT%2CEPS%2CBPS%2CROE%2CTOTAL_SHARES%2CFREE_SHARES%2CFREE_CAP&quoteColumns=f2%2Cf3&quoteType=0&source=WEB&client=WEB&filter=(TYPE%3D%221%22)"
+// ZZ500 返回中证500成分股列表
+func (e EastMoney) ZZ500(ctx context.Context) (results []ZZ500Item, err error) {
+	apiurl := "https://datacenter-web.eastmoney.com/api/data/v1/get?sortColumns=ROE&sortTypes=-1&pageSize=500&pageNumber=1&reportName=RPT_INDEX_TS_COMPONENT&columns=SECUCODE%2CSECURITY_CODE%2CTYPE%2CSECURITY_NAME_ABBR%2CCLOSE_PRICE%2CINDUSTRY%2CREGION%2CWEIGHT%2CEPS%2CBPS%2CROE%2CTOTAL_SHARES%2CFREE_SHARES%2CFREE_CAP&quoteColumns=f2%2Cf3&quoteType=0&source=WEB&client=WEB&filter=(TYPE%3D%223%22)"
 	header := map[string]string{
 		"user-agent": uarand.GetRandom(),
 	}
-	rsp := RspHS300{}
+	rsp := RspZZ500{}
 	if err := goutils.HTTPGET(ctx, e.HTTPClient, apiurl, header, &rsp); err != nil {
 		return nil, err
 	}
 	if rsp.Code != 0 {
-		return nil, fmt.Errorf("HS300 rsp code error, rsp:%+v", rsp)
+		return nil, fmt.Errorf("ZZ500 rsp code error, rsp:%+v", rsp)
 	}
-	if len(rsp.Result.Data) != 300 {
-		return nil, fmt.Errorf("HS300 rsp data len != 300, len=%d", len(rsp.Result.Data))
+	if len(rsp.Result.Data) != 500 {
+		return nil, fmt.Errorf("ZZ500 rsp data len != 500, len=%d", len(rsp.Result.Data))
 	}
 	return rsp.Result.Data, nil
 }
