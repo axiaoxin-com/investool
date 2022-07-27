c1881cae223cc1055cabb21f40cd2e6be58bbfef
axiaoxin <254606826@qq.com>
2022-07-27 15:05:07 +0800

    clean release

--- release.sh
+++ release.sh
@@ -1,16 +1,4 @@
 #!/bin/bash
-# 依赖：
-# npm install uglify-js -g
-# go get -u github.com/swaggo/swag/cmd/swag
-# go get github.com/x-motemen/gobump/cmd/gobump
-# qshell https://github.com/qiniu/qshell/releases/download/v2.6.2/qshell-v2.6.2-linux-amd64.tar.gz
-# qshell account <Your AccessKey> <Your SecretKey> <Your Name>
-go mod tidy && \
-go test -race ./... && \
-rm -rf ./statics/js/app.min.*.js && \
-bash ./misc/scripts/app.min.js.sh && \
-bash ./misc/scripts/bumpversion.sh && \
-echo "push..."
-git push && git push --tags -f && \
+# git tag first
 echo "release..."
 goreleaser release --rm-dist
